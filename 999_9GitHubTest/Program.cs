af73b603ce7978006767c9ca3e27478b28e59d6b
ERKAN TUNCAY KARA <erkantuncaykara@gmail.com>
2021-01-28 13:29:25 +0300

    Add project files.

---
+++ 999_9GitHubTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _999_9GitHubTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
